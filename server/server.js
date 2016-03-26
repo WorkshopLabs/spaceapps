23c39a1a9fade0240e2a3288fabb208eaad1657a
Rene Polo <renejpolo@me.com>
2016-03-26 01:25:46 -0400

    first push

--- server/server.js
+++ server/server.js
@@ -1,13 +1,33 @@
+var subdomain = require('express-subdomain');
 var express = require('express');
 var app = express();
 var path = require('path');
-
-
+var router = express.Router();
 var port = process.env.PORT || 3030;
 
+//api specific routes 
+router.get('/', function(req, res) {
+    res.send('Welcome to our API!');
+});
+ 
+router.get('/users', function(req, res) {
+    res.json([
+        { name: "Brian" }
+    ]);
+});
 
-app.use(express.static(path.join(__dirname, '../public')));
 
+
+
+app.get('/', function(req, res) {
+    res.send('Homepage');
+});
+
+
+// app.use(express.static(path.join(__dirname, '../public')));
+
+
+app.use(subdomain('space', router));
 
 
 app.listen( port, function(){

dc2c0095f882caf56768ad7aba4fda3b676e16b0
Rene Polo <renejpolo@me.com>
2016-04-13 18:59:40 -0400

    grafico added

--- server/server.js
+++ server/server.js
@@ -3,25 +3,25 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var router = express.Router();
+var router2 = express.Router();
 var port = process.env.PORT || 3030;
 
 //api specific routes 
 router.get('/', function(req, res) {
    res.sendFile(path.join(__dirname, '../public/space.html'));
 });
- 
-router.get('/users', function(req, res) {
-    res.json([
-        { name: "Brian" }
-    ]);
+
+router.get('/', function(req, res) {
+   res.sendFile(path.join(__dirname, '../public/grafico.html'));
 });
 
 
 // app.get('/', function(req, res) {
-// res.sendFile(path.join(__dirname, '../public/space.html'));
+// res.sendFile(path.join(__dirname, '../public/grafico.html'));
 // });
 
 app.use(subdomain('space', router));
+app.use(subdomain('grafico', router2));
 
 app.use(express.static(path.join(__dirname, '../public')));
 
